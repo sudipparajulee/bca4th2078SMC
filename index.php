f7d5e561def6d165f053db508af5840512708b73
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-02-09 06:31:06 +0545

    test

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
     <div class="grid grid-cols-4 gap-10 px-20">
         <div class="bg-gray-100 p-5 rounded shadow">
-            <h1>Product 1</h1>
+            <h1>Product 1</h1> 
         </div>
 
         <div class="bg-gray-100 p-5 rounded shadow">
