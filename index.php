c1202634fb6c9328fb8397d51461e542cd22cbb9
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-02-09 06:29:58 +0545

    project

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1 class="text-center font-bold text-4xl my-10">Our <span class="text-red-500"> Products </span></h1>
+
+    <div class="grid grid-cols-4 gap-10 px-20">
+        <div class="bg-gray-100 p-5 rounded shadow">
+            <h1>Product 1</h1>
+        </div>
+
+        <div class="bg-gray-100 p-5 rounded shadow">
+            <h1>Product 1</h1>
+        </div>
+
+        <div class="bg-gray-100 p-5 rounded shadow">
+            <h1>Product 1</h1>
+        </div>
+
+        <div class="bg-gray-100 p-5 rounded shadow">
+            <h1>Product 1</h1>
+        </div>
+    </div>
+</body>
+</html>
