1e606b92070dc73f986f26f88f8a653512b4ba07
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-02-13 06:24:49 +0545

    connection

--- admin/actioncategory.php
+++ admin/actioncategory.php
@@ -4,5 +4,20 @@ $priority = $_POST['priority'];
 
 $qry = "INSERT INTO categories(name, priority) VALUES('$name', $priority)";
 
-echo $qry;
+//create connection
+$server = "localhost";
+$username = "root";
+$password = "";
+$db = "bcaecommerce";
+
+$conn = mysqli_connect($server, $username, $password, $db);
+
+//check connection
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+else{
+    echo "Connected successfully";
+}
+
 ?>
