9cc97dce53abf8c55588613dd7dbdcd051f4ec7b
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-02-15 07:35:51 +0545

    update and delete

--- admin/actioncategory.php
+++ admin/actioncategory.php
@@ -1,18 +1,64 @@
 <?php
-$name = $_POST['name'];
-$priority = $_POST['priority'];
+if(isset($_POST['store']))
+{
+    $name = $_POST['name'];
+    $priority = $_POST['priority'];
 
-$qry = "INSERT INTO categories(name, priority) VALUES('$name', $priority)";
+    $qry = "INSERT INTO categories(name, priority) VALUES('$name', $priority)";
 
-include '../includes/dbconnection.php';
-$result = mysqli_query($conn, $qry);
-if($result){
-    echo "<script>
-    alert('Category added successfully');
-    window.location.href = 'categories.php';
-    </script>";
-}else{
-    echo "Error adding category";
+    include '../includes/dbconnection.php';
+    $result = mysqli_query($conn, $qry);
+    if($result){
+        echo "<script>
+        alert('Category added successfully');
+        window.location.href = 'categories.php';
+        </script>";
+    }else{
+        echo "Error adding category";
+    }
+    include '../includes/closeconnection.php';
 }
-include '../includes/closeconnection.php';
+
+//update
+if(isset($_POST['update']))
+{
+    $id = $_POST['categoryid'];
+    $name = $_POST['name'];
+    $priority = $_POST['priority'];
+
+    $qry = "UPDATE categories SET name = '$name', priority = $priority WHERE id = $id";
+
+    include '../includes/dbconnection.php';
+    $result = mysqli_query($conn, $qry);
+    include '../includes/closeconnection.php';
+
+    if($result){
+        echo "<script>
+        alert('Category updated successfully');
+        window.location.href = 'categories.php';
+        </script>";
+    }else{
+        echo "Error updating category";
+    }
+}
+
+//delete
+if(isset($_GET['deleteid']))
+{
+    $id = $_GET['deleteid'];
+    $qry = "DELETE FROM categories WHERE id = $id";
+
+    include '../includes/dbconnection.php';
+    $result = mysqli_query($conn, $qry);
+    include '../includes/closeconnection.php';
+
+    if($result){
+        echo "<script>
+        alert('Category deleted successfully');
+        window.location.href = 'categories.php';
+        </script>";
+    }else{
+        echo "Error deleting category";
+    }
+}
 ?>
--- admin/categories.php
+++ admin/categories.php
@@ -28,7 +28,7 @@ include '../includes/closeconnection.php';
             <td class="border p-2"><?php echo $row['name']; ?></td>
             <td class="border p-2">
                 <a href="editcategory.php?id=<?php echo $row['id']; ?>" class="bg-blue-600 text-white px-4 mx-1 py-1 rounded">Edit</a>
-                <a href="" class="bg-red-600 text-white px-4 mx-1 py-1 rounded">Delete</a>
+                <a href="actioncategory.php?deleteid=<?php echo $row['id'];?>" class="bg-red-600 text-white px-4 mx-1 py-1 rounded" onclick="return confirm('Are you sure to Delete?');">Delete</a>
             </td>
         </tr>
         <?php
