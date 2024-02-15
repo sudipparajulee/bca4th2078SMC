d13e16b14f4a1f61b5573d9536c1a4646e96c9e2
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-02-15 06:31:45 +0545

    updates

--- admin/editcategory.php
+++ admin/editcategory.php
@@ -11,8 +11,8 @@ $row = mysqli_fetch_assoc($result);
     <hr class="my-3 h-1 bg-orange-500">
 
     <form action="actioncategory.php" method="POST">
-        <input type="text" name="priority" placeholder="Enter Priority" class="p-2 bg-gray-100 border rounded w-full block my-3">
-        <input type="text" name="name" placeholder="Enter Category Name" class="p-2 bg-gray-100 border rounded w-full block my-3">
+        <input type="text" name="priority" value="<?php echo $row['priority']; ?>" placeholder="Enter Priority" class="p-2 bg-gray-100 border rounded w-full block my-3">
+        <input type="text" name="name" value="<?php echo $row['name']; ?>" placeholder="Enter Category Name" class="p-2 bg-gray-100 border rounded w-full block my-3">
         
         <div class="my-5 flex justify-center gap-5">
             <input type="submit" value="Add Category" class="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
