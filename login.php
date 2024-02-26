2f7bac0d0d215512a692dec0d0247e38321ad023
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-02-26 07:41:16 +0545

    login and register

--- login.php
+++ login.php
@@ -1,13 +1,38 @@
 <?php include 'includes/header.php'; ?>
 
     <div class="flex justify-center items-center my-10">
-        <form action="admin/dashboard.php" class="bg-gray-100 p-10 rounded shadow">
+        <form action="" method="POST" class="bg-gray-100 p-10 rounded shadow">
             <h1 class="text-center font-bold text-4xl my-10">Login</h1>
-            <input type="text" class="w-full p-2 my-5 border-2 border-gray-300" placeholder="Username">
-            <input type="password" class="w-full p-2 my-5 border-2 border-gray-300" placeholder="Password">
-            <button type="submit" class="w-full p-2 my-5 bg-orange-500 text-white font-bold">Login</button>
+            <input type="text" class="w-full p-2 my-5 border-2 border-gray-300" name="email" placeholder="Email">
+            <input type="password" class="w-full p-2 my-5 border-2 border-gray-300" name="password" placeholder="Password">
+            <button type="submit" name="login" class="w-full p-2 my-5 bg-orange-500 text-white font-bold">Login</button>
+            <div class="my-5">
+                <p class="text-center">Don't have an account? <a href="register.php" class="text-blue-500">Register Now</a></p>
+            </div>
         </form>
+        
     </div>
 
 
 <?php include 'includes/footer.php'; ?>
+
+<?php
+if(isset($_POST['login']))
+{
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password = md5($password);
+    $qry = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    include 'includes/dbconnection.php';
+    $result = mysqli_query($conn, $qry);
+    include 'includes/closeconnection.php';
+    if(mysqli_num_rows($result) > 0)
+    {
+        $row = mysqli_fetch_assoc($result);
+        header('location: admin/dashboard.php');
+    }
+    else
+    {
+        echo "<script>alert('Invalid Email or Password')</script>";
+    }
+}
---
+++ register.php
@@ -0,0 +1,44 @@
+<?php include 'includes/header.php';?>
+    <!-- Register  -->
+
+    <div class="flex justify-center items-center my-10">
+        <form action="" method="POST" class="bg-gray-100 p-10 rounded shadow">
+            <h1 class="text-center font-bold text-4xl my-10">Register</h1>
+            <input type="text" class="w-full p-2 my-5 border-2 border-gray-300" name="fullname" placeholder="Full Name">
+            <input type="email" class="w-full p-2 my-5 border-2 border-gray-300" name="email" placeholder="Email">
+            <input type="password" class="w-full p-2 my-5 border-2 border-gray-300" name="password" placeholder="Password">
+            <input type="password" class="w-full p-2 my-5 border-2 border-gray-300" name="cpassword" placeholder="Confirm Password">
+            <button type="submit" name="register" class="w-full p-2 my-5 bg-orange-500 text-white font-bold">Register</button>
+            <div class="my-5">
+                <p class="text-center">Already have an account? <a href="login.php" class="text-blue-500">Login Now</a></p>
+            </div>
+        </form>
+    </div>
+
+<?php include 'includes/footer.php';?>
+
+<?php
+if(isset($_POST['register'])){
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+
+    if($password == $cpassword){
+        $password = md5($password);
+        $qry = "INSERT INTO users (fullname, email, password) VALUES ('$fullname', '$email', '$password')";
+        include 'includes/dbconnection.php';
+        $result = mysqli_query($conn, $qry);
+        include 'includes/closeconnection.php';
+        if($result){
+            echo "<script>alert('User Registered Successfully');
+            window.location.href = 'login.php';
+            </script>";
+        }else{
+            echo "<script>alert('User Registration Failed')</script>";
+        }
+    }else{
+        echo "<script>alert('Password and Confirm Password do not match')</script>";
+    }
+}
+?>
